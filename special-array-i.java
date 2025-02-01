e39479e82a84902194df997e10b726d5ac58777f
Balaji Saravanan <balajilitsv@gmail.com>
2025-02-01 23:13:54 +0530

    Create special-array-i.java

---
+++ special-array-i.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/special-array-i
+
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        int parity = nums[0] % 2;
+        
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] % 2 != parity) {
+                parity = nums[i] % 2;
+                continue;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
